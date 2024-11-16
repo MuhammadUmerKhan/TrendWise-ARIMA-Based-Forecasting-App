6c17ad4bf1d6712283a12bd802e9c3b20761991f
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2024-11-16 14:46:07 +0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,8 @@ import statsmodels.api as sm
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.arima.model import ARIMA
 import plotly.graph_objects as go
-
-import plotly.graph_objects as go
-from statsmodels.tsa.stattools import adfuller
-from statsmodels.tsa.arima.model import ARIMA
-import statsmodels.api as sm
+import itertools
+from sklearn.metrics import mean_squared_error
 
 def TimeSeries_graph(ts, model='additive', decomposition_plot=True):
     fig_ts = go.Figure()
@@ -59,7 +56,7 @@ def TimeSeries_graph(ts, model='additive', decomposition_plot=True):
             line=dict(color='#1f78b4', width=3)
         ))
         fig_observed.update_layout(
-            title="📊 Observed",
+            title="📉 Observed",
             xaxis_title="Time",
             yaxis_title="Values",
             plot_bgcolor="#222",
@@ -142,9 +139,51 @@ def adf_test(series, max_diff=10, alpha=0.05):
     result_df['p-value'] = result_df['p-value'].astype(str)
     return result_df, stationary_data
 
-def ARIMA_model(original_series):
-    final_model = ARIMA(original_series, order=(6, 0, 0)).fit()
-    prediction = final_model.predict(start=1600, end=len(original_series) + 30)
+def ARIMA_model(original_series, search_for_params = False, stationary_series = None):
+    if search_for_params:
+        #  Train-Test Split
+        train_len = int(len(stationary_series) * 0.8)
+        train = stationary_series[:train_len]
+        test = stationary_series[train_len:]
+
+        mse_lst, rmse_lst, order_lst = [], [], []
+        pdq_combinations = list(itertools.product(range(0, 8), range(0, 8), range(0, 2)))
+
+        print("Please wait, it may take a few moments...")
+        print("Searching for the best ARIMA model parameters...")
+
+        for pdq in pdq_combinations:
+            try:
+                model = ARIMA(train, order=pdq).fit()
+                pred = model.predict(start=len(train), end=len(stationary_series) - 1)
+
+                # Metrics Calculations
+                mse = mean_squared_error(test, pred)
+                rmse = np.sqrt(mse)
+
+                # Appending to Lists
+                order_lst.append(pdq)
+                mse_lst.append(mse)
+                rmse_lst.append(rmse)
+
+            except Exception as e:
+                print(f"Error with ARIMA order {pdq}: {e}")
+                continue
+
+        # Create DataFrame for Results
+        result_df = pd.DataFrame({
+            'Mean Squared Error': mse_lst,
+            'Root Mean Squared Error': rmse_lst,
+        }, index=order_lst).sort_values(ascending=True, by='Mean Squared Error').head(1)
+
+        best_params = result_df.index[0]
+        
+        # Final Model Training with Best Parameters
+        final_model = ARIMA(original_series, order=best_params).fit()
+        prediction = final_model.predict(start=1600, end=len(original_series) + 30)
+    else:
+        final_model = ARIMA(original_series, order=(6, 0, 0)).fit()
+        prediction = final_model.predict(start=1600, end=len(original_series) + 30)
 
     fig = go.Figure()
 
@@ -183,9 +222,9 @@ def ARIMA_model(original_series):
 
 # Set the page configuration for a more appealing look
 st.set_page_config(
-    page_title="📊 ARIMA Time Series Forecasting",
+    page_title="📉 ARIMA Time Series Forecasting",
     page_icon="📉",
-    layout="wide",
+    # layout="wide",
     initial_sidebar_state="expanded"
 )
 
@@ -289,27 +328,37 @@ st.markdown(
     """, unsafe_allow_html=True
 )
 
-st.markdown('<div class="intro-title">📊 ARIMA Time Series Forecasting 📊</div>', unsafe_allow_html=True)
+st.markdown('<div class="intro-title">📉 ARIMA Time Series Forecasting 📉</div>', unsafe_allow_html=True)
 st.markdown('<div class="intro-subtitle">This tool allows you to visualize time series data, check stationarity with the Augmented Dickey-Fuller test 📉</div>', unsafe_allow_html=True)
 
-
-tab1, tab2, tab3 = st.tabs(["🏠 Home", "📋 Yahoo Stock Price Forecasting", " 📂 Ipload your dataset"])
+# Tabs for better organization
+tab1, tab2, tab3 = st.tabs(["🏠 Home", "📋 Yahoo Stock Price Forecasting", "📂 Upload your dataset"])
 # Main Code
 
 with tab1:
     st.markdown('<div class="section-title">👋 About Me</div>', unsafe_allow_html=True)
-    st.markdown('<div class="content">Hi! I\'m Muhammad Umer Khan, an aspiring Data Scientist with a passion for Natural Language Processing (NLP). Currently pursuing my Bachelor’s in Computer Science, I have hands-on experience with projects in data science, data scraping, and building intelligent recommendation systems.</div>', unsafe_allow_html=True)
+    st.markdown("""
+        <div class="content">
+            Hi! I'm Muhammad Umer Khan, an aspiring Data Scientist with a passion for Natural Language Processing (NLP). Currently pursuing my Bachelor's in Computer Science, I have hands-on experience with projects in data science, data scraping, and building intelligent recommendation systems.
+        </div>
+    """, unsafe_allow_html=True)
+    st.markdown('<div class="section-title">📢 <strong>Note:</strong></div>', unsafe_allow_html=True)
+    st.markdown("""
+        <div class="content">
+            In the <strong style="color: #FF5733;">Upload Your Dataset</strong> section, please upload clean data.
+        </div>
+    """, unsafe_allow_html=True)
 
     st.markdown('<div class="section-title">🔍 Project Overview</div>', unsafe_allow_html=True)
     st.markdown("""
         <div class="content">
-            This project focuses on developing an <span class="highlight">Intelligent Stock Price Prediction System</span> time series techniques. The system aims to predict stock prices based on historical data. Here's a breakdown of the project:
+            This project focuses on developing an <span class="highlight">Intelligent Stock Price Prediction System</span> using time series techniques. Here's a breakdown of the project:
             <ul>
-                <li><span class="highlight">Data Collection 📊</span>: Gathered <a href="https://www.kaggle.com/datasets/blastchar/telco-customer-churn" target="_blank" style="color: #2980B9;">Historical stock data</a> and news articles for training the model.</li>
-                <li><span class="highlight">Feature Engineering 🔧</span>: Extracted key features like moving averages, stock volatility, and sentiment scores from news data.</li>
-                <li><span class="highlight">Model Development 🤖</span>: Built machine learning models such as LSTM (Long Short Term Memory) and Random Forest for stock price prediction.</li>
-                <li><span class="highlight">Evaluation 📊</span>: Used metrics like RMSE and MAPE to evaluate model performance and improve accuracy.</li>
-                <li><span class="highlight">Deployment 🌐</span>: Deployed the model using Streamlit to provide real-time stock predictions with an intuitive interface.</li>
+                <li>📊 <span class="highlight">Data Collection</span>: Gathered historical stock data and news articles.</li>
+                <li>🔍 <span class="highlight">Feature Engineering</span>: Extracted key features like Date, High, Low, Open, Close, Volume, etc.</li>
+                <li>📈 <span class="highlight">Model Development</span>: Built ARIMA models and used hypothesis testing like ADF.</li>
+                <li>🔬 <span class="highlight">Evaluation</span>: Evaluated using metrics like RMSE and MAPE.</li>
+                <li>🌐 <span class="highlight">Deployment</span>: Streamlit for an intuitive web interface.</li>
             </ul>
         </div>
     """, unsafe_allow_html=True)
@@ -317,30 +366,110 @@ with tab1:
     st.markdown('<div class="section-title">💻 Technologies Used</div>', unsafe_allow_html=True)
     st.markdown("""
         <div class="content">
-            - <span class="highlight">Languages & Libraries</span>: Python, Pandas, NumPy, Scikit-Learn, Keras, TensorFlow, SpaCy, and Streamlit<br>
-            - <span class="highlight">Deployment</span>: Streamlit for creating a user-friendly web interface and model deployment.
+            - <span class="highlight">Languages & Libraries</span>: Python, Pandas, NumPy, Statsmodels, Plotly, and Streamlit<br>
+            - <span class="highlight">Deployment</span>: Streamlit for creating a user-friendly web interface.
         </div>
     """, unsafe_allow_html=True)
 
 with tab2:
+    st.markdown('<div class="section-title">📋 Forecast Stock Prices</div>', unsafe_allow_html=True)
+    st.markdown("""
+        <div class="content">
+            Use this tab to upload your dataset or select default data to visualize stock prices and forecast future trends.
+        </div>
+    """, unsafe_allow_html=True)
     data = pd.read_csv("./Data/Yahoo Stock Data (correct format).csv")
 
     if data is not None:
         columns_with_placeholder = ["Please Select"] + list(data.columns)
 
-
-
         # Index column selection
         index_column = st.selectbox("Select the Index Column 📅", columns_with_placeholder)
         if index_column != "Please Select":
-            try:
-                # Convert to datetime
-                data[index_column] = pd.to_datetime(data[index_column], errors="coerce")
-                if data[index_column].isna().any():
-                    st.error("The selected column contains invalid date values. Please clean your data.")
-                else:
-                    data.set_index(index_column, inplace=True)
-                    st.success("Index column successfully set as datetime.")
+            if np.issubdtype(data[index_column].dtype, np.datetime64) or data[index_column].dtype == object:
+                try:
+                    # Convert to datetime
+                    temp_column = pd.to_datetime(data['Date'], format = '%Y-%m-%d')
+                    data[index_column] = temp_column
+                    data.set_index(index_column, inplace=True)  
+                    
+                    # data.index = data.index.strftime('%Y-%m-%d')                 
+                    st.success("✅ Index column successfully set as datetime with proper frequency.")
+
+                    # Time series column selection
+                    time_series_columns = ["Please Select"] + list(data.columns)
+                    time_series_col = st.selectbox("Select the Time Series Column (Values)", time_series_columns)
+
+                    if time_series_col != "Please Select":
+                        # Validate numeric column
+                        if np.issubdtype(data[time_series_col].dtype, np.number):
+                            # Session state management
+                            if 'first_function_done' not in st.session_state:
+                                st.session_state.first_function_done = False
+                            if 'second_function_done' not in st.session_state:
+                                st.session_state.second_function_done = False
+                            if 'third_function_done' not in st.session_state:
+                                st.session_state.third_function_done = False
+
+                            st.table(data[[time_series_col]].sample(5))
+                            # Step 1: Time series graph
+                            if st.button("Generate Time Series Graph 📈"):
+                                if not st.session_state.first_function_done:
+                                    TimeSeries_graph(data[[time_series_col]])
+                                    st.session_state.first_function_done = True
+                                    st.success("✅ Time Series Graph Generated! Move to the next step.")
+                                
+                            # Step 2: Augmented Dickey-Fuller test
+                            if st.session_state.first_function_done and not st.session_state.second_function_done:
+                                if st.button("Run Augmented Dickey-Fuller Test 🧪"):
+                                    adf_result, stationary_data = adf_test(data[[time_series_col]])
+                                    if stationary_data is not None:
+                                        st.write(adf_result)
+                                        st.session_state.second_function_done = True
+                                        st.success("✅ ADF Test Completed! Move to the next step.")
+                                    else:
+                                        st.warning("⚠️ ADF test failed to make the series stationary.")
+
+                            # Step 3: ARIMA model
+                            if st.session_state.second_function_done and not st.session_state.third_function_done:
+                                if st.button("Run ARIMA Model ⚙️"):
+                                    ARIMA_model(data[[time_series_col]])
+                                    st.session_state.third_function_done = True
+                                    st.success("✅ ARIMA Model Completed!")
+                        else:
+                            st.error("🚫 The selected column is not numeric. Please select a valid numeric column.")
+                    else:
+                        st.warning("⚠️ Please select a time series column.")
+                except Exception as e:
+                    st.error("🚫 Error processing the selected column: {e}")
+            else:
+                st.warning("⚠️ The selected column '{index_column}' appears to be of type {data[index_column].dtype}. It's not recommended to convert it to datetime.")
+
+        else:
+            st.warning("⚠️ Please select an index column.")
+
+with tab3:
+    
+    st.markdown('<div class="section-title"><strong style="color: #FF5733;">📤 Upload your Cleaned CSV file here!</strong>', unsafe_allow_html=True)
+    uploaded_file = st.file_uploader("", type=["csv"])
+
+     
+      
+    if uploaded_file is not None:
+        st.success("✅ ✅ File uploaded successfully! Processing data...")
+        data = pd.read_csv(uploaded_file)
+        columns_with_placeholder = ["Please Select"] + list(data.columns)
+
+        # Index column selection
+        index_column = st.selectbox("Select the Index Column 📅", columns_with_placeholder, key="index_column_key")
+        if index_column != "Please Select":
+            if np.issubdtype(data[index_column].dtype, np.datetime64) or data[index_column].dtype == object:
+                try:
+                    # Convert to datetime
+                    temp_column = pd.to_datetime(data[index_column])
+                    data[index_column] = temp_column
+                    data.set_index(index_column, inplace=True)                   
+                    st.success("✅ Index column successfully set as datetime with proper frequency.")
 
                     # Time series column selection
                     time_series_columns = ["Please Select"] + list(data.columns)
@@ -358,43 +487,45 @@ with tab2:
                                 st.session_state.third_function_done = False
 
                             # Step 1: Time series graph
-                            if st.button("Generate Time Series Graph"):
+                            if st.button("Generate Time Series Graph 📈"):
                                 if not st.session_state.first_function_done:
                                     TimeSeries_graph(data[[time_series_col]])
                                     st.session_state.first_function_done = True
-                                    st.success("Time Series Graph Generated! Move to the next step.")
+                                    st.success("✅ Time Series Graph Generated! Move to the next step.")
                                 
                             # Step 2: Augmented Dickey-Fuller test
                             if st.session_state.first_function_done and not st.session_state.second_function_done:
-                                if st.button("Run Augmented Dickey-Fuller Test"):
+                                if st.button("Run Augmented Dickey-Fuller Test 🧪"):
                                     adf_result, stationary_data = adf_test(data[[time_series_col]])
                                     if stationary_data is not None:
                                         st.write(adf_result)
                                         st.session_state.second_function_done = True
-                                        st.success("ADF Test Completed! Move to the next step.")
+                                        st.success("✅ ADF Test Completed! Move to the next step.")
                                     else:
-                                        st.warning("ADF test failed to make the series stationary.")
+                                        st.warning("⚠️ ADF test failed to make the series stationary.")
 
                             # Step 3: ARIMA model
                             if st.session_state.second_function_done and not st.session_state.third_function_done:
-                                if st.button("Run ARIMA Model"):
-                                    ARIMA_model(data[[time_series_col]])
+                                if st.button("Run ARIMA Model ⚙️"):
+                                    _, stationary_data = adf_test(data[[time_series_col]])
+                                    ARIMA_model(data[[time_series_col]], search_for_params=True, stationary_series=stationary_data)
                                     st.session_state.third_function_done = True
-                                    st.success("ARIMA Model Completed! Move to the next step.")
+                                    st.success("✅ ARIMA Model Completed!")
                         else:
-
-                            st.error("The selected column is not numeric. Please select a valid numeric column.")
-
+                            st.error("🚫 The selected column is not numeric. Please select a valid numeric column.")
                     else:
-
-                        st.warning("Please select a time series column.")
-
-            except Exception as e:
-
-                st.error(f"Error processing the selected column: {e}")
+                        st.warning("⚠️ Please select a time series column.")
+                except Exception as e:
+                    st.error("🚫 Error processing the selected column: {e}")
+            else:
+                st.warning("⚠️ The selected column '{index_column}' appears to be of type {data[index_column].dtype}. It's not recommended to convert it to datetime.")
 
         else:
+            st.info("💡 Please upload a CSV file to start analysis.")
 
-            st.warning("Please select an index column.")
-with tab3:
-    st.write("Please upload your file below:")
+# Footer
+st.markdown("""
+    <div class="footer">
+        Developed by <a href="https://portfolio-sigma-mocha-67.vercel.app/" target="_blank" style="color: #2980B9;">Muhammad Umer Khan</a>. Powered by Streamlit and Statsmodel 🌐
+    </div>
+""", unsafe_allow_html=True)
